f7f408531a11427a908074490fda0aa17badfb0d
ElisKina-dev <elis.kina@nttdata.com>
2024-08-01 11:27:52 +0200

    Bank repo modify

--- src/main/java/it/gov/pagopa/atmlayer/service/userservice/repository/BankRepository.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/userservice/repository/BankRepository.java
@@ -9,6 +9,7 @@ import it.gov.pagopa.atmlayer.service.userservice.entity.BankEntity;
 import it.gov.pagopa.atmlayer.service.userservice.model.PageInfo;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,6 +18,29 @@ import java.util.stream.Collectors;
 public class BankRepository implements PanacheRepositoryBase<BankEntity, String> {
 
     public Uni<PageInfo<BankEntity>> findByFilters(Map<String, Object> params, int pageIndex, int pageSize) {
+        StringBuilder query = new StringBuilder("select b from BankEntity b where 1=1 ");
+        Map<String, Object> queryParams = new HashMap<>();
+
+        params.forEach((key, value) -> {
+            query.append("and lower(b.").append(key).append(") LIKE lower(concat(concat(:").append(key).append(", '%'), '%')) ");
+            queryParams.put(key, "%" + value + "%");
+        });
+
+        query.append("order by b.lastUpdatedAt DESC");
+
+        PanacheQuery<BankEntity> queryResult = find(query.toString(), queryParams).page(Page.of(pageIndex, pageSize));
+
+        return queryResult.count()
+                .onItem().transformToUni(count -> {
+                    int totalCount = count.intValue();
+                    int totalPages = (int) Math.ceil((double) totalCount / pageSize);
+                    return queryResult.list()
+                            .onItem().transform(list -> new PageInfo<>(pageIndex, pageSize, totalCount, totalPages, list));
+                });
+    }
+
+
+    /*public Uni<PageInfo<BankEntity>> findByFilters(Map<String, Object> params, int pageIndex, int pageSize) {
         String queryFilters = params.keySet().stream()
                 .map(key -> "b." + key + " = :" + key)
                 .collect(Collectors.joining(" and "));
@@ -35,7 +59,7 @@ public class BankRepository implements PanacheRepositoryBase<BankEntity, String>
                             .transform(list -> new PageInfo<>(pageIndex, pageSize, totalCount, totalPages, list));
                 });
     }
-
+*/
 
     public Uni<List<BankEntity>> findAllById(String acquirerId) {
         String sql = "SELECT * FROM bank WHERE acquirer_id = :acquirerId order by last_updated_at DESC";
