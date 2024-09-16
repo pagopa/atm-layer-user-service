5459bd07d9548d1f91725eb467847921859d8c54
EmVal <emanuele.valentini@nttdata.com>
2024-09-16 18:23:14 +0200

    Added integration tests files

---
+++ src/test/resources/integration-test/run-postman.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+newman run /postman/Integration_test_console_service_3giu.postman_collection.json -r cli,json,htmlextra \
+--reporter-json-export output/result.json --reporter-htmlextra-export output/result.html \
+--env-var "baseUrl=http://host.testcontainers.internal:8086"
