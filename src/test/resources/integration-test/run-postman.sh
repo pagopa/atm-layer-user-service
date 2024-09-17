79a8358754deeb65b551d4217be801a692ef66a2
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-09-17 10:43:01 +0200

    integration tests

--- src/test/resources/integration-test/run-postman.sh
+++ src/test/resources/integration-test/run-postman.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-newman run /postman/Integration_test_console_service_3giu.postman_collection.json -r cli,json,htmlextra \
+newman run /postman/Integration_Tests_User_Service.postman_collection_17sep.json -r cli,json,htmlextra \
 --reporter-json-export output/result.json --reporter-htmlextra-export output/result.html \
 --env-var "baseUrl=http://host.testcontainers.internal:8086"
